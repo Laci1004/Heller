fbc0b773c739b1bd23fd5cee6fa4db411004f7a5
Laci1004 <78487658+Laci1004@users.noreply.github.com>
2021-02-16 19:32:36 +0100

    Add files via upload

--- feladat_1.py
+++ feladat_1.py
@@ -1,5 +1,5 @@
-for i in range(1, 101):
-    if i % 3 ==0:
-        print(i)
+for i in range(1, 101):   #defining range 1-100
+    if i % 3 ==0:         #calculates numbers divisible by 3
+        print(i)          #print numbers divisibly by 3
 
 
